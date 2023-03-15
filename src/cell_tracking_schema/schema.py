61297928be0af418b3e08529377aee7fd31c709b
Ko Sugawara <ko.sugawara@ens-lyon.fr>
2023-03-15 23:46:12 +0100

    Add networkx interfacing to GraphModel Also removes the now deprecated `axis_order` and its validation From: https://github.com/Janelia-Trackathon-2023/sequitur/commit/557ca68

--- src/cell_tracking_schema/schema.py
+++ src/cell_tracking_schema/schema.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from pydantic import BaseModel, root_validator
+import networkx as nx
 
 
 class RequiredFieldsMixin:
@@ -31,19 +32,24 @@ class StrTuple(List[str]):
         return tuple(str(v) for v in values)
 
 
-class NodeModel(RequiredFieldsMixin, BaseModel):
+class NodeModel(BaseModel):
     node_id: int
-    coordinates: FloatTuple  # can be an empty tuple
+
+    # coordinates: FloatTuple  # can be an empty tuple
+    t: Optional[float] = None
+    x: Optional[float] = None
+    y: Optional[float] = None
+    z: Optional[float] = None
     score: Optional[float] = None
 
     class Config:
         extra = "allow"
 
 
-class EdgeModel(RequiredFieldsMixin, BaseModel):
+class EdgeModel(BaseModel):
     edge_id: int
-    src_id: int
-    dst_id: int
+    source_id: int
+    target_id: int
     score: Optional[float] = None
 
     class Config:
@@ -53,26 +59,39 @@ class EdgeModel(RequiredFieldsMixin, BaseModel):
 class GraphModel(BaseModel):
     nodes: List[NodeModel]
     edges: List[EdgeModel]
-    axis_order: StrTuple  # can be an empty tuple
 
     @root_validator
     def validate(cls, values):
-        nodes: List[NodeModel] = values.get("nodes")
-        edges: List[EdgeModel] = values.get("edges")
-        axis_order = values.get("axis_order")
-        for node in nodes:
-            if len(node.coordinates) != len(axis_order):
-                raise ValueError
+        nodes = values.get("nodes")
+        edges = values.get("edges")
         node_ids = set(node.node_id for node in nodes)
-        edge_node_set = set(edge.src_id for edge in edges).union(
-            set(edge.dst_id for edge in edges)
+        edge_node_set = set(edge.source_id for edge in edges).union(
+            set(edge.target_id for edge in edges)
         )
         if not edge_node_set.issubset(node_ids):
             raise ValueError
         return values
 
+    def as_networkx(self):
+        g = nx.DiGraph()
+        g.add_nodes_from([(node.node_id, node) for node in self.nodes])
+        g.add_edges_from(
+            [(edge.source_id, edge.target_id, edge) for edge in self.edges]
+        )
+        return g
 
-class TrackModel(GraphModel):
+    @classmethod
+    def parse_networkx(cls, graph):
+        nodes = [NodeModel.parse_obj(obj[1]) for obj in graph.nodes(data=True)]
+        edges = [EdgeModel.parse_obj(obj[2]) for obj in graph.edges(data=True)]
+        graph_model = cls(
+            nodes=nodes,
+            edges=edges,
+        )
+        return graph_model
+
+
+class TrackModel(BaseModel):
     track_id: int
     start_id: int
     end_id: int
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -12,12 +12,12 @@ from cell_tracking_schema.schema import (
 
 @pytest.fixture
 def node_json():
-    return '{"node_id": 0, "coordinates": [1.0, 2.0], "score": 0.5}'
+    return '{"node_id": 0, "x": 1.0, "y": 2.0, "z": 3.0, "score": 0.5}'
 
 
 @pytest.fixture
 def edge_json():
-    return '{"edge_id": 0, "src_id": 0, "dst_id": 1, "score": 0.5}'
+    return '{"edge_id": 0, "source_id": 0, "target_id": 1, "score": 0.5}'
 
 
 # ----------------------------------------------
@@ -49,40 +49,40 @@ def test_str_tuple():
 
 
 def test_node_model():
-    node = NodeModel(node_id=0, coordinates=(1.0, 2.0), score=0.5)
+    node = NodeModel(node_id=0, x=1.0, y=2.0, z=3.0, score=0.5)
     assert node.node_id == 0
-    assert node.coordinates == (1.0, 2.0)
+    assert node.x == 1.0
+    assert node.y == 2.0
+    assert node.z == 3.0
     assert node.score == 0.5
 
 
 def test_node_model_no_node_id():
     with pytest.raises(ValidationError):
-        NodeModel(coordinates=(1.0, 2.0))
-
-
-def test_node_model_no_coordinates():
-    with pytest.raises(ValidationError):
-        NodeModel(node_id=0)
+        NodeModel(x=1.0)
 
 
 def test_node_model_no_score():
-    node = NodeModel(node_id=0, coordinates=(1.0, 2.0))
+    node = NodeModel(node_id=0, x=1.0, y=2.0, z=3.0)
     assert node.node_id == 0
-    assert node.coordinates == (1.0, 2.0)
+    assert node.x == 1.0
+    assert node.y == 2.0
+    assert node.z == 3.0
     assert node.score is None
 
 
 def test_node_model_from_json(node_json):
     node = NodeModel.parse_raw(node_json)
     assert node.node_id == 0
-    assert node.coordinates == (1.0, 2.0)
     assert node.score == 0.5
 
 
 def test_node_model_extra():
-    node = NodeModel(node_id=0, coordinates=(1.0, 2.0), extra="extra")
+    node = NodeModel(node_id=0, x=1.0, y=2.0, z=3.0, extra="extra")
     assert node.node_id == 0
-    assert node.coordinates == (1.0, 2.0)
+    assert node.x == 1.0
+    assert node.y == 2.0
+    assert node.z == 3.0
     assert node.score is None
     assert node.extra == "extra"
 
@@ -93,49 +93,49 @@ def test_node_model_extra():
 
 
 def test_edge_model():
-    edge = EdgeModel(edge_id=0, src_id=0, dst_id=1, score=0.5)
+    edge = EdgeModel(edge_id=0, source_id=0, target_id=1, score=0.5)
     assert edge.edge_id == 0
-    assert edge.src_id == 0
-    assert edge.dst_id == 1
+    assert edge.source_id == 0
+    assert edge.target_id == 1
     assert edge.score == 0.5
 
 
 def test_edge_model_no_edge_id():
     with pytest.raises(ValidationError):
-        EdgeModel(src_id=0, dst_id=1)
+        EdgeModel(source_id=0, target_id=1)
 
 
-def test_edge_model_no_src_id():
+def test_edge_model_no_source_id():
     with pytest.raises(ValidationError):
-        EdgeModel(edge_id=0, dst_id=1)
+        EdgeModel(edge_id=0, target_id=1)
 
 
-def test_edge_model_no_dst_id():
+def test_edge_model_no_target_id():
     with pytest.raises(ValidationError):
-        EdgeModel(edge_id=0, src_id=0)
+        EdgeModel(edge_id=0, source_id=1)
 
 
 def test_edge_model_no_score():
-    edge = EdgeModel(edge_id=0, src_id=0, dst_id=1)
+    edge = EdgeModel(edge_id=0, source_id=0, target_id=1)
     assert edge.edge_id == 0
-    assert edge.src_id == 0
-    assert edge.dst_id == 1
+    assert edge.source_id == 0
+    assert edge.target_id == 1
     assert edge.score is None
 
 
 def test_edge_model_from_json(edge_json):
     edge = EdgeModel.parse_raw(edge_json)
     assert edge.edge_id == 0
-    assert edge.src_id == 0
-    assert edge.dst_id == 1
+    assert edge.source_id == 0
+    assert edge.target_id == 1
     assert edge.score == 0.5
 
 
 def test_edge_model_extra():
-    edge = EdgeModel(edge_id=0, src_id=0, dst_id=1, extra="extra")
+    edge = EdgeModel(edge_id=0, source_id=0, target_id=1, extra="extra")
     assert edge.edge_id == 0
-    assert edge.src_id == 0
-    assert edge.dst_id == 1
+    assert edge.source_id == 0
+    assert edge.target_id == 1
     assert edge.score is None
     assert edge.extra == "extra"
 
@@ -146,34 +146,26 @@ def test_edge_model_extra():
 
 
 def test_graph_model(node_json, edge_json):
-    nodes = [
-        NodeModel(node_id=0, coordinates=(1.0, 2.0), score=0.5),
-        NodeModel(node_id=1, coordinates=(2.0, 3.0), score=0.5),
+    nodes = [NodeModel(node_id=i, x=1.0, y=2.0, z=3.0, score=0.5) for i in range(10)]
+    edges = [
+        EdgeModel(edge_id=i, source_id=i, target_id=i + 1, score=0.5) for i in range(9)
     ]
-    edges = [EdgeModel(edge_id=0, src_id=0, dst_id=1, score=0.5)]
-    graph = GraphModel(nodes=nodes, edges=edges, axis_order=("X", "Y"))
-    assert graph.nodes[0].node_id == 0
-    assert graph.nodes[0].coordinates == (1.0, 2.0)
-    assert graph.nodes[0].score == 0.5
-    assert graph.edges[0].edge_id == 0
-    assert graph.edges[0].src_id == 0
-    assert graph.edges[0].dst_id == 1
-    assert graph.edges[0].score == 0.5
-    assert graph.axis_order == ("X", "Y")
-
-
-def test_graph_model_coordinates_axis_order_mismatch():
-    nodes = [
-        NodeModel(node_id=0, coordinates=(1.0, 2.0), score=0.5),
-        NodeModel(node_id=1, coordinates=(2.0, 3.0), score=0.5),
-    ]
-    edges = [EdgeModel(edge_id=0, src_id=0, dst_id=1, score=0.5)]
-    with pytest.raises(ValidationError):
-        GraphModel(nodes=nodes, edges=edges, axis_order=("X",))
+    graph = GraphModel(nodes=nodes, edges=edges)
+    for i, node in enumerate(graph.nodes):
+        assert node.node_id == i
+        assert node.x == 1.0
+        assert node.y == 2.0
+        assert node.z == 3.0
+        assert node.score == 0.5
+    for i, edge in enumerate(graph.edges):
+        assert graph.edges[0].edge_id == 0
+        assert graph.edges[0].source_id == 0
+        assert graph.edges[0].target_id == 1
+        assert graph.edges[0].score == 0.5
 
 
 def test_graph_model_node_edge_mismatch():
-    nodes = [NodeModel(node_id=0, coordinates=(1.0, 2.0), score=0.5)]
-    edges = [EdgeModel(edge_id=0, src_id=0, dst_id=1, score=0.5)]
+    nodes = [NodeModel(node_id=0, x=1.0, y=2.0, z=3.0, score=0.5)]
+    edges = [EdgeModel(edge_id=0, source_id=0, target_id=1, score=0.5)]
     with pytest.raises(ValidationError):
-        GraphModel(nodes=nodes, edges=edges, axis_order=("X", "Y"))
+        GraphModel(nodes=nodes, edges=edges)
